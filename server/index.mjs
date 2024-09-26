56c818840b03985e73536c202e5a5ca08909f2a4
Farid Mahmudov <razormad666@gmail.com>
2024-09-26 16:24:04 +0300

    first commit

---
+++ server/index.mjs
@@ -0,0 +1,29 @@
+import http from 'http'
+import fs from 'fs'
+import path from 'path'
+
+
+const __dirname = path.resolve('./')
+const PORT = 3002
+
+const app = http.createServer((req, res) => {
+
+    if(req.url === '/favicon.ico') {
+       
+        return
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile(path.join(__dirname, '/client/index.html'), 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        res.end(data)
+
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})

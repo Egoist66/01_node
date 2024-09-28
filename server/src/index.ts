44442d9670706e269fa7208c8e10ad73c28cb508
Farid Mahmudov <razormad666@gmail.com>
2024-09-28 15:17:08 +0300

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,42 +1,39 @@
-import http from 'http'
-import fs from 'fs'
-import path from 'path'
+import http from "http";
+import fs from "fs";
+import path from "path";
+import { emitters } from "./models/emitters.js";
 
 
 
 
-const __dirname = path.resolve('./')
-const PORT: number = 3003
+const __dirname = path.resolve("./");
+const PORT: number = 3003;
 
 const app = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") {
+    return;
+  }
 
-    if(req.url === '/favicon.ico') {
-       
-        return
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile(
+    path.join(__dirname, "/client/index.html"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      res.end(`${data}`);
     }
-
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile(path.join(__dirname, '/client/index.html'), 'utf8', (err, data) => {
-        if (err) {
-            console.log(err)
-            return
-        }
-
-        res.end(`${data}`)
-
-    })
-})
+  );
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+
+    //   emitters();
 
 
 
-fs.stat(path.join(__dirname, '/client/index.html'), (err, stats) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-  
-})
+});
